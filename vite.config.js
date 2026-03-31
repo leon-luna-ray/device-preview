f6d74be4042f37e141e13f52553645107b3a21fb
Ray Luna <ray@hello-upswell.com>
2026-03-30 17:47:17 -0700

    add wrangler

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
-// import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // tailwindcss(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
